30262a1c76f425c315d7488a94e0e42cbf0011c9
Vladimir Roncevic <elektron.ronca@gmail.com>
2017-01-23 11:14:43 +0100

    Update debug, error, configuration, logging and notify mechanism

--- bin/pinger.pl
+++ bin/pinger.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 #
 # @brief    Ping operation and logging statistics
 # @version  ver.1.0
@@ -8,15 +8,16 @@
 #
 use strict;
 use warnings;
+use Pod::Usage;
 use Sys::Hostname;
 use Getopt::Long;
-use Pod::Usage;
 use File::Basename qw(dirname);
 use Cwd qw(abs_path);
 use lib dirname(dirname(abs_path($0))) . '/../../lib/perl5';
-use Logging;
-use Configuration;
-use Notification;
+use InfoDebugMessage qw(info_debug_message);
+use Logging qw(logging);
+use Configuration qw(read_preference);
+use Notification qw(notify);
 use Status;
 
 my $cfg = dirname(dirname(abs_path($0))) . "/conf/pinger.cfg";
@@ -34,75 +35,70 @@ our $TOOL_DBG="false";
 # pinger()
 #
 sub pinger {
-	my $fcaller = (caller(0))[3];
-	my $host = hostname();
-	my %preferences;
-	my $preferences_ref = \%preferences;
-	my $status = 1;
-	$status = readpref($cfg, $preferences_ref);
-	if($status == $NOT_SUCCESS) {
-		exit(128);
-	}
-	my $ping_cmd = "ping -c5 " . $preferences{TARGET_HOST};
-	my %notification;
-	my $notification_ref=\%notification;
-	$notification{ADMIN_EMAIL}=$preferences{ADMIN_EMAIL};
-	$notification{EMAIL_FROM}="pinger\@$host";
-	my %logger;
-	my $logger_ref = \%logger;
-	$logger{LOG_FILE_PATH}=$log;
-	open(PING_STUFF,"$ping_cmd|");
-	while (my $lyne = <PING_STUFF>) {
-		if ($lyne =~ /time=(\S+)/) {
-		    my $elt = $1;
-		    my $log_line = "ping to $preferences{TARGET_HOST}";
-			$logger{LOG_MESSAGE}="$log_line $elt ms";
-			$status = logging($logger_ref);
-			if($status == $NOT_SUCCESS) {
+	my ($host, $pingCmd, $msg);
+	my (%prefStruct, %notStruct, %logStruct);
+	if(read_preference($cfg, \%prefStruct) == $SUCCESS) {
+		$pingCmd = "ping -c5 " . $prefStruct{TARGET_HOST};
+		$notStruct{ADMIN_EMAIL} = $prefStruct{ADMIN_EMAIL};
+		$host = hostname();
+		$notStruct{EMAIL_FROM} = "pinger\@$host";
+		$logStruct{LOG_FILE_PATH} = $log;
+		$msg = "Start ping command";
+		info_debug_message($msg);
+		open(PING_STUFF,"$pingCmd |");
+		my ($lyne, $elt, $logLine);
+		while($lyne = <PING_STUFF>) {
+			if($lyne =~ /time=(\S+)/) {
+				$elt = $1;
+				$logLine = "ping to $prefStruct{TARGET_HOST}";
+				$logStruct{LOG_MESSAGE} = "$logLine $elt ms";
+				if(logging(\%logStruct) == $SUCCESS) {
+					if($elt > 300) {
+						$notStruct{MESSAGE} = $logStruct{LOG_MESSAGE};
+						if(notify(\%notStruct) == $SUCCESS) {
+							next;
+						}
+						exit(130);
+					}
+					next;
+				}
 				exit(129);
 			}
-		    if ($elt > 300) {
-		        $notification{MESSAGE}=$logger{LOG_MESSAGE};
-		        $status = notify($notification_ref);
-		        if($status == $NOT_SUCCESS) {
-					exit(130);
-		        }
-		    } 
+			next;
 		}
+		$msg = "Done";
+		info_debug_message($msg);
+		exit(0);
 	}
-	my $msg = "Done";
-	if($TOOL_DBG eq "true") {
-		print("[Info] " . $fcaller . " " . $msg . "\n");
-	}
-	exit(0);
+	exit(128);
 }
 
 #
 # @brief   Main entry point
 # @param   Value optional help | manual
 # @exitval Script tool pinger exit with integer value
-#			0   - success operation 
+#			0   - success operation
 # 			127 - run as root user
-# 			128 - failed to load Configuration
-# 			129 - failed to write log message 
-# 			130 - failed to send email
+# 			128 - failed to load configuration from CFG file
+# 			129 - failed to write log message to LOG file
+# 			130 - failed to send email notification
 #
 my $help = 0;
 my $man = 0;
 
-if (@ARGV > 0) {
+if(@ARGV > 0) {
 	GetOptions(
 		'help|?' => \$help,
 		'manual' => \$man) or pod2usage(2);
 }
 
-if ($man || $help) {
-    if($help) {
-        pod2usage(1);
-    }
-    if($man) {
-        pod2usage(VERBOSE => 2);
-    }
+if($man || $help) {
+	if($help) {
+		pod2usage(1);
+	}
+	if($man) {
+		pod2usage(VERBOSE => 2);
+	}
 }
 
 my $username = (getpwuid($>));
@@ -114,6 +110,7 @@ if(($username eq "root") && ($uid == 0)) {
 
 exit(127);
 __END__
+
 ################################ POD pinger.pl #################################
 
 =head1 NAME
@@ -124,17 +121,17 @@ pinger - ping operation and logging statistics
 
 Use:
 
-    pinger [options] 
+	pinger [options] 
 
 Examples:
 
-    progen help
+	pinger help
 
-    progen man
+	pinger man
 
-    #	Ping operation and logging statistics
+	# Ping operation and logging statistics
 
-    pinger
+	pinger
 
 =head1 DESCRIPTION
 
@@ -148,13 +145,13 @@ pinger takes the following arguments:
 
 =item help
 
-  help
+	help
 
 (Optional.) Show help info information
 
 =item manual
 
-  help
+	help
 
 (Optional.) Display manual information
 
@@ -162,8 +159,8 @@ pinger takes the following arguments:
 
 =head1 AUTHORS
 
-Ullrich Nake, E<lt>ullrich.nake@frobas.comE<gt>.
-Vladimir Roncevic, E<lt>vladimir.roncevic@frobas.comE<gt>.
+Ullrich Nake, E<lt>ullrich.nake@frobas.comE<gt>
+Vladimir Roncevic, E<lt>vladimir.roncevic@frobas.comE<gt>
 
 =head1 COPYRIGHT
 
